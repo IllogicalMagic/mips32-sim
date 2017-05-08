d0a0517f0ea1b07a0f9112cf7406c5e2160be5e8
IllogicalMagic <coillol@yandex.ru>
2017-05-08 03:00:56 +0300

    Added some useful common headers

---
+++ common/op_traits.h
@@ -0,0 +1,16 @@
+#ifndef SIM_MIPS32_COMMON_OPTRAITS_HEADER__
+#define SIM_MIPS32_COMMON_OPTRAITS_HEADER__
+
+namespace Simulator {
+
+namespace OpTraits {
+
+  enum class OpType {
+    // For now it is empty.
+  };
+
+} // namespace OpTypes
+
+} // namespace Simulator
+
+#endif

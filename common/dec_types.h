a6099689ad12199d6b5a0d1bf7a73bd9f7f9bffc
Sergey <cega97@gmail.com>
2017-05-14 16:27:04 +0300

    To sync master and Decoder

---
+++ common/dec_types.h
@@ -0,0 +1,34 @@
+#ifndef SIM_MIPS32_COMMON_DEC_TYPES_HEADER__
+#define SIM_MIPS32_COMMON_DEC_TYPES_HEADER__
+
+#include "types.h"
+
+namespace Simulator {
+
+namespace Types {
+
+  typedef size_t regn_t;
+  typedef word_t imm_t;
+
+  namespace OpTypes {
+  
+  enum OpType {
+    #include "opnum.h"
+    OpNum
+  };
+  
+  }
+
+  struct Insn {
+    OpTypes::OpType op;
+    regn_t rs;
+    regn_t rt;
+    regn_t rd;
+    imm_t imm;
+  };
+
+} // namespace Types
+
+} // namespace Simulator
+
+#endif //SIM_MIPS32_COMMON_DEC_TYPES_HEADER__

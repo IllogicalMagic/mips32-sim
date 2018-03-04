344c34aa4b731b39b1897e2085fe5e8e85e2752c
IllogicalMagic <coillol@yandex.ru>
2018-03-04 22:56:39 +0300

    Add more debug info for instruction handlers

--- execution/process_insn.py
+++ execution/process_insn.py
@@ -11,7 +11,7 @@ handlers_cxx = 'handlers.cpp'
 out_c = 'insn_handlers.inc'
 
 insns = {}
-debug_line = '  PRINT_DEBUG("Executed {0}\\n");\n'
+debug_line = '  PRINT_DEBUG("{0}: \\t");\n'
 handler_stub = ' {{\n  PRINT_DEBUG("Executed {0}\\n");\n  assert(0 && "Unimplemented insn");\n}}\n'
 handler_decl = 'void processInsn{0}(const Types::Insn &i)'
 
@@ -21,14 +21,19 @@ repl_table = [ ('GPR', 'registerMap'),
                ('rt' , 'i.rt'),
                ('rd' , 'i.rd'),
                ('imm', 'i.imm'),
-               ('S.' , 'sysregs.'),
-               ('.u' , '.uVal'),
-               ('.s' , '.sVal'),
+               ('S\.' , 'sysregs.'),
+               ('\.u' , '.uVal'),
+               ('\.s' , '.sVal'),
                ('w_t', 'word_t'),
                ('b_t', 'byte_t'),
                ('r_shift', 'arithmetic_rshift'),
                ('AccType::', 'MMU::AccType::'),
                ('memory', 'memory.get()')]
+
+defuse_table = [ 'GPR\[r.\]',
+                 'HI',
+                 'LO',
+                 'imm']
 
 def process_line(line):
     global insns
@@ -53,10 +58,21 @@ def extract_line(h):
 
 def replace_tokens(line):
     for old, new in repl_table:
-        line = line.replace(old, new)
+        line = re.sub(old + '(?!!)', new, line)
+    for old, new in repl_table:
+        line = re.sub(old + '!', old, line)
     return line
 
-def process_def(h, insn):
+# expand all special variables into corresponding C++ code
+def process_def(insn):
+    global insns
+    new_hb = []
+    for line in insns[insn]:
+        new_hb.append(replace_tokens(line))
+    insns[insn] = new_hb
+
+# read handler from first { to last }
+def read_handler(h, insn):
     line = extract_line(h)
 
     while line == '\n':
@@ -72,11 +88,107 @@ def process_def(h, insn):
 
     line = extract_line(h)
     while line != '}\n':
-        handler_body.append(replace_tokens(line))
+        handler_body.append(line)
         line = extract_line(h)
+
     handler_body.append(line)
 
     insns[insn] = handler_body
+
+# filter non-conditional statements from handlers
+def filter_stmts(handler):
+    stmts_raw = handler.translate(None, '\n')
+    # remove complex if statements
+    stmts_raw = re.sub('.* {.*}', '', handler)
+    stmts = stmts_raw.split(';\n')
+    # get only assignments
+    return filter(lambda x : ' = ' in x, stmts)
+
+# return pair of (type, index) for value
+def extract_index(val):
+    if 'GPR' in val:
+        reg = re.search('\[(.*)\]', val)
+        if not reg:
+            print "Bad register"
+            raise Exception
+        return ('${0}!=$%zu'.format(reg.group(1)), reg.group(1))
+    if 'imm' in val:
+        return ('imm!', None)
+    if 'HI' in val:
+        return ('HI', None)
+    if 'LO' in val:
+        return ('LO', None)
+
+# return PRINT_DEBUG with format string and arguments
+def get_debug_string(prefix, fmts, args):
+    if args:
+        res = '  PRINT_DEBUG("{0}{1}",'.format(prefix, ', '.join(fmts))
+        res = res + ', '.join(args)
+        res = res + ');\n'
+    else:
+        res = ''
+    return res
+
+# generate debug string for tracing defs and uses
+def gen_full_info(defs, uses, before):
+    args = []
+    fmts = []
+    for d in defs:
+        fmt, arg = extract_index(d)
+        fmts.append(fmt + '(0x%08x)')
+        if arg:
+            args.append(arg)
+        args.append(d)
+    if before:
+        for u in uses:
+            fmt, arg = extract_index(u)
+            fmts.append(fmt + '(0x%08x)')
+            if arg:
+                args.append(arg)
+            args.append(u)
+
+    prefix = ''
+    if not before:
+        prefix = 'WB: '
+    return get_debug_string(prefix, fmts, args)
+
+# append .u for values that should be somehow accessed
+def get_ref_unsigned(ref):
+    if 'GPR' in ref or 'HI' in ref or 'LO' in ref:
+        return ref + '.u'
+    return ref
+
+def gen_printer(insn):
+    handler = ''.join(insns[insn][2:-1])
+    stmts = filter_stmts(handler)
+    defs = set()
+    uses = set()
+    for stmt in stmts:
+        x = stmt.split(' = ')
+        lhs, rhs = stmt.split(' = ')
+        for var in defuse_table:
+            matched_lhs = re.findall(var, lhs)
+            defs = defs.union(matched_lhs)
+            matched_rhs = re.findall(var, rhs)
+            uses = uses.union(matched_rhs)
+
+    defs = map(get_ref_unsigned, defs)
+    uses = map(get_ref_unsigned, uses)
+
+    before = gen_full_info(defs, uses, True)
+    after = gen_full_info(defs, uses, False)
+    if after != '':
+        before = before + 'PRINT_DEBUG("; ");\n'
+    after = after + '  PRINT_DEBUG("\\n");\n'
+    res = insns[insn]
+    # insert after "Executed ..."
+    res.insert(2, before)
+    # insert before last }
+    if 'DelaySlot' in res[-2]:
+        res.insert(-2, after)
+    else:
+        res.insert(-1, after)
+    insns[insn] = res
 
 def process_handlers():
     h = open(handlers_cxx, 'r')
@@ -90,7 +202,9 @@ def process_handlers():
                 insn = defm.group(1)
                 if insn not in insns:
                     print 'Unknown insn {0}'.format(insn)
-                process_def(h, defm.group(1))
+                read_handler(h, insn)
+                gen_printer(insn)
+                process_def(insn)
 
         line = h.readline()
 
